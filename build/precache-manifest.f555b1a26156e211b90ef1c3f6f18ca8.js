271cfbc7fa88a56800711b6124a9a198a33c401e
Jeetujitender <39960180+Jeetujitender@users.noreply.github.com>
2021-04-20 14:52:37 +0530

    Add files via upload

---
+++ build/precache-manifest.f555b1a26156e211b90ef1c3f6f18ca8.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "27bdbb1f3f160d8c980ee4b9bbe3de7e",
+    "url": "/index.html"
+  },
+  {
+    "revision": "f791a2f685074419420d",
+    "url": "/static/css/main.856d8521.chunk.css"
+  },
+  {
+    "revision": "78a19f65bffd8604b2e7",
+    "url": "/static/js/2.29ba2828.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.29ba2828.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f791a2f685074419420d",
+    "url": "/static/js/main.69b28418.chunk.js"
+  },
+  {
+    "revision": "b1820bfc6ade90caaf5b",
+    "url": "/static/js/runtime-main.51c4ed28.js"
+  }
+]);
